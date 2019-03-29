9dc82c042666d10c9ccbf7cc3d251d54ee414761
henrikedstrand <henrik997@icloud.com>
2019-03-29 12:14:49 +0100

    added circle

---
+++ Circle.java
@@ -0,0 +1,17 @@
+//Test
+
+public class Circle {
+
+	private int radius;
+	public Ciricle(){
+		radius=10;
+	}
+
+	public void setSize(int newSize){
+		raduis=newSize;
+	}
+
+	public int getSize(){
+		return radius;
+	}
+}
